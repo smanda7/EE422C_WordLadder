79800de76dc5df51c7fa529cd10a651629cfa895
Siva Manda <sivamanda@gmail.com>
2016-09-27 14:28:15 -0500

    Added header

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
- * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
+ * Siva Manda
+ * sm48525
+ * 16480
  * <Student2 Name>
  * <Student2 EID>
  * <Student2 5-digit Unique No.>
  * Slip days used: <0>
- * Git URL:
+ * Git URL: https://github.com/smanda7/EE422C_WordLadder
  * Fall 2016
  */
 
@@ -43,6 +42,7 @@ public class Main {
 		// initialize your static variables or constants here.
 		// We will call this method before running our JUNIT tests.  So call it 
 		// only once at the start of main.
+		String [] array1; 
 	}
 	
 	/**
